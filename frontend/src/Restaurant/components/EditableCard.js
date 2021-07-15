1e0192ed3417b81aef2d67b48cf357a4e8e284e5
Fenil Shah <fn777214@dal.ca>
2021-07-15 14:52:56 +0530

    added menu configuration page for the restaurant

---
+++ frontend/src/Restaurant/components/EditableCard.js
@@ -0,0 +1,11 @@
+import {Navbar,Nav,Form,FormControl,Button} from 'react-bootstrap';
+
+function EditableCard() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default EditableCard;
---
+++ frontend/src/Restaurant/components/Navbar.js
@@ -0,0 +1,25 @@
+import {Navbar,Nav,Form,FormControl,Button} from 'react-bootstrap';
+
+function AppNavBar() {
+  return (
+    <div className="App">
+      <Navbar collapseOnSelect expand="lg" bg="dark"  variant="dark">
+            <Navbar.Brand href="/restaurantHome">
+                {/* <img src={logo} className="d-inline-block align-top" height="150"/> */}
+                RestaurantName <sub>HalifaxFoodie</sub>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="container-fluid">
+                    <Nav.Link href="/restaurantMenu">Menu</Nav.Link>
+                    <Nav.Link href="/promocodes">Promocodes</Nav.Link>
+                    <Nav.Link href="/recipes">Recipe</Nav.Link>
+                    <Nav.Link eventKey={2} href="/logout" className="ml-auto">Logout</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default AppNavBar;
